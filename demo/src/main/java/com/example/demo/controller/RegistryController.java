c5ca5445e5212667876fb04885c09e4c6cecf2dd
beethovenvirusd <80507473+beethovenvirusd@users.noreply.github.com>
2021-03-23 21:54:12 +0800

    Delete demo directory

--- demo/src/main/java/com/example/demo/controller/RegistryController.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-
-@Controller
-public class RegistryController {
-}
